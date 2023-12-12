88e23405a2858fc267f7eb6b77b6bf690e073ee9
Eduard Talovikov <44552193+TEROMO@users.noreply.github.com>
2023-12-13 03:12:41 +0600

    Update custom_list_tile.dart

--- lib/components/custom_list_tile.dart
+++ lib/components/custom_list_tile.dart
@@ -4,62 +4,55 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class CustomListTile extends StatelessWidget {
-  const CustomListTile(
-      {super.key, required this.data});
+  const CustomListTile({super.key, required this.data});
 
   final CustomTileModel data;
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
-        onPressed: () => {},
-        child: SizedBox(
-          height: 64,
-          child: Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      onPressed: () => {},
+      child: SizedBox(
+        height: 64,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
+                Padding(
+                    padding: const EdgeInsets.only(right: 12),
+                    child: SvgPicture.asset(
+                      data.ico,
+                      height: 36,
+                      width: 36,
+                    )),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Padding(
-                        padding: const EdgeInsets.only(right: 12),
-                        child: SvgPicture.asset(
-                          data.ico,
-                          height: 36,
-                          width: 36,
-                        )
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          data.title,
-                          style: MyTextStyles.customTileTitle
-                        ),
-                        data.subtitle != "" ? Text(
-                          data.subtitle,
-                          style: MyTextStyles.customTileSubtitle
-                        ): const SizedBox(height: 0,) ,
-                      ],
-                    )
+                    Text(data.title, style: MyTextStyles.customTileTitle),
+                    data.subtitle != ""
+                        ? Text(data.subtitle, style: MyTextStyles.customTileSubtitle)
+                        : const SizedBox(
+                            height: 0,
+                          ),
                   ],
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(right: 8),
-                  child: Icon(
-                    Icons.arrow_forward_ios,
-                    color: Colors.grey,
-                    size: 14,
-                  ),
                 )
               ],
             ),
-          ),
+            const Padding(
+              padding: EdgeInsets.only(right: 8),
+              child: Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.grey,
+                size: 14,
+              ),
+            )
+          ],
         ),
+      ),
     );
-
   }
 }
